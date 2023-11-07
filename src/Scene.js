9222bfb9bfffb48ff65e2a6bbede016ea89884ef
yungshun317 <seraph317@gmail.com>
2023-11-07 11:14:05 +0800

    [FEAT] Define the React Three Fiber Scene in the `Canvas` object

---
+++ src/Scene.js
@@ -0,0 +1,4 @@
+
+const Scene = () => {};
+
+export default Scene;
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import { Canvas } from "@react-three/fiber";
+import Scene from "./Scene";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const createCanvasHandler = (state) => {
+    state.gl.setClearColor("cyan", 0.5);
+};
+
+root.render(
+    <>
+        <Canvas
+            gl={{
+                antialias: true,
+                alpha: true
+            }}
+
+            // Orthographic
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 100,
+                // zoom: 120,
+                position: [2, 2, 6]
+            }}
+
+            onCreated={createCanvasHandler}
+        >
+            <Scene />
+        </Canvas>
+    </>
+);
