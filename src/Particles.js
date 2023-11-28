f806b6e2fc90f8cf4c661d6e7b74be0972f2a099
yungshun317 <seraph317@gmail.com>
2023-11-28 14:20:11 +0800

    [FEAT] Create particles

--- src/Particles.js
+++ src/Particles.js
@@ -1,6 +1,36 @@
 import * as THREE from "three";
+import {useRef} from "react";
+import {useFrame} from "@react-three/fiber";
 
 const Particles = () => {
+    const particles = useRef();
+
+    useFrame((_, delta) => {
+        particles.current.rotation.y += delta * 0.1;
+        particles.current.rotation.x += delta * 0.1;
+    })
+
+    const verticesAmount = 2000;
+    const positionArray = new Float32Array(verticesAmount * 3);
+
+    for (let i = 0; i < verticesAmount * 3; i++) {
+        positionArray[i] = (Math.random() - 0.5) * 10.0;
+    }
+
+    return (
+        <points ref={particles}>
+            <bufferGeometry>
+                <bufferAttribute
+                    attach="attributes-position"
+                    count={positionArray.length / 3}
+                    itemSize={3}
+                    array={positionArray}
+                />
+            </bufferGeometry>
+        </points>
+    );
+
+    /*
     const positionArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
 
     return (
@@ -16,6 +46,7 @@ const Particles = () => {
             <meshBasicMaterial color="purple" side={THREE.DoubleSide} />
         </mesh>
     );
+    */
 };
 
 export default Particles;
